6a1e273e12ff37a4863cc264f3eef876c165b4ff
Luiz Felipe Ribeiro Paixão <luizfelipe10049@gmail.com>
2022-09-24 16:31:33 -0300

    Estrutura básica do template

---
+++ app/config/routes.js
@@ -0,0 +1,16 @@
+//Jeito que o angular faz a injeção de dependencias
+(function(){
+angular.module('sistemaCustoLogistico').config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    //'$httpProvider'
+    function($stateProvider, $urlRouterProvider, $httpProvider) {
+        $stateProvider.state('dashboard', {url: "/dashboard",templateUrl: "dashboard/dashboard.html"})
+                      .state('usuario', {url: "/usuario", templateUrl: "usuario/usuario.html"})
+
+        //$httpProvider.interceptors.push('handleResponseError')
+
+        $urlRouterProvider.otherwise('/dashboard')
+    }
+]) 
+})()
